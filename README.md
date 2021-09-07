ed7e909169ae6c5dae99a8142daa7876f6dd87fa
Melvin Snijders <mail@melvinsnijders.nl>
2021-09-07 12:37:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Windesheim Glade to CSV Chrome extension
+Convert a Windesheim Glade (Amazing Challenge SE) to a readable and parsable CSV file.
+This is a chrome extension that adds an export button to all glades in the Amazing Challenge of Windesheim SE.
+
+![Screenshot](https://img.melvinsnijders.nl/gVjjCmqK)
+
+## CSV Syntax
+
+| letter | object         | number                                          |
+| ------ | -------------- | ----------------------------------------------- |
+| q      | obstacle       |
+| x      | bomb           | seconds (steps) till explosions                 |
+| t      | target (doel)  | number of target                                |
+| m      | money (bonus)  | 2 ^ x is the bonus gained                       |
+| d      | turner (draai) | 1-3 times turning to the left, 0 is random turn |
+| s      | start          | 0-3 is direction, clock-wise with 0 being north |
+| w      | white square   |
+| g      | gray square    |
+| r      | red square     |
+| o      | orange square  |
+| y      | yellow square  |
+| e      | green square   |
+| b      | blue square    |
+| p      | purple square  |
+| l      | black square   |
---
+++ src/content-script.js
@@ -0,0 +1,71 @@
+let button = document.createElement("button");
+button.innerHTML = "Export naar CSV";
+button.classList.add("btn", "btn-success");
+
+let insertLocation = document.getElementsByName("Glade")[0];
+insertLocation.prepend(button);
+
+const colors = ["l", "p", "b", "e", "y", "o", "r", "g", "w"]
+
+button.addEventListener("click", function () {
+    let items = Array.from(document.getElementsByClassName("field-container")[0].querySelectorAll(".field-block"))
+        .map((item) => {
+            let child = item.firstChild;
+            let type = child.getAttribute('type');
+            const number = child.getAttribute('value');
+
+            switch (type) {
+                case "wood": case "bush": case "stone":
+                    return "q";
+                case "bomb":
+                    return "x" + number;
+                case "goal":
+                    return "t" + number
+                case "bonus":
+                    return "m" + number;
+                case "turn":
+                    return "d" + number;
+                case "start":
+                    return "s" + number
+                case "color":
+                    return colors[number];
+            }
+
+        });
+
+    const rows = [];
+    while (items.length > 0) {
+        const chunk = items.splice(0, 20);
+        rows.push(chunk);
+    }
+
+    let csvContent = "data:text/csv;charset=utf-8,"
+        + rows.map(e => e.join(";")).join("\n");
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "glade.csv");
+    document.body.appendChild(link);
+
+    link.click();
+
+});
+
+/**
+q	obstacle
+x	bomb	seconds (steps) till explosions
+t	target (doel)	number of target
+m	money (bonus)	2 ^ x is the bonus gained
+d	turner (draai)	1-3 times turning to the left, 0 is random turn
+s	start	0-3 is direction, clock-wise with 0 being north
+w	white square
+g	gray square
+r	red square
+o	orange square
+y	yellow square
+e	green square
+b	blue square
+p	purple square
+l	black square
+ */
